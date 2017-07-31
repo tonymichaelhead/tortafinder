e151890b37ab8996879627224ca13087dbcb71de
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-31 12:37:12 -0700

    Setup react boilerplates and webpack config

---
+++ dev/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from 'react-dom';
+ 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  
+  render() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var DEV = path.resolve(__dirname, "dev");
+var OUTPUT = path.resolve(__dirname, "output");
+
+var config = {
+    entry: DEV + "/index.jsx",
+    output: {
+        path: OUTPUT,
+        filename: "mycode.js"
+    },
+    module: {
+        loaders: [{
+            include: DEV,
+            loader: 'babel-loader',
+        }]
+    }
+};
+
+module.exports = config;
+
+//./node_modules/.bin/webpack --watch
